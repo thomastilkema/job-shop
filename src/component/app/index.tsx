eb1064d196819f7bbd1fbc6f58d970024c53c1b1
Thomas <thomastilkema@@users.noreply.github.com>
2018-08-06 01:13:17 +0200

    feat(app-component): add route for the confirm page

--- src/component/app/index.tsx
+++ src/component/app/index.tsx
@@ -3,17 +3,19 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import '@app/style/app.css';
 
+import ConfirmCheckoutPage from '@app/component/page/confirm';
 import VacanciesPage from '@app/component/page/vacancies';
 
 class Component extends React.Component<{}, {}> {
 
   public render() {
     return (
-      <div className="container">
-        <BrowserRouter>
+      <BrowserRouter>
+        <div className="container">
           <Route exact={true} path="/" component={VacanciesPage} />
-        </BrowserRouter>
-      </div>
+          <Route path="/bevestigen" component={ConfirmCheckoutPage} />
+        </div>
+      </BrowserRouter>
     );
   }
 
--- src/component/app/spec.tsx
+++ src/component/app/spec.tsx
@@ -2,6 +2,7 @@ import { shallow, ShallowWrapper } from 'enzyme';
 import * as React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import ConfirmPage from '@app/component/page/confirm';
 import VacanciesPage from '@app/component/page/vacancies';
 import App from './';
 
@@ -18,16 +19,21 @@ describe('the app component', () => {
     instance = shallow(getComponent());
   });
 
-  it('should wrap all content within a container', () => {
-    expect(instance.hasClass('container')).toBe(true);
-  });
-
   it('should have support for routes', () => {
     expect(instance.find(BrowserRouter).exists()).toBe(true);
   });
 
+  it('should wrap all content within a container', () => {
+    const browserRouterElement = instance.find(BrowserRouter);
+    expect(browserRouterElement.childAt(0).hasClass('container')).toBe(true);
+  });
+
   it('should add a route for the vacancies page', () => {
     expect(instance.contains(<Route exact={true} path="/" component={VacanciesPage} />)).toBe(true);
+  });
+
+  it('should add a route for the confirm page', () => {
+    expect(instance.contains(<Route path="/bevestigen" component={ConfirmPage} />)).toBe(true);
   });
 
 });

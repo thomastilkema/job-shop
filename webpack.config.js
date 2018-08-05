a02292713618d74861d08b52cbddc794ecc71069
Thomas <thomastilkema@@users.noreply.github.com>
2018-08-05 15:09:13 +0200

    build(css): add dependencies to support importing styles during build

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,11 @@ module.exports = {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
     ]
   },
 
